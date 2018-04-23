08171fc740d4eff7d6ac5742945e131e9d4a0361
Aron Dadi <aron.d.dadi@gmail.com>
2018-04-23 16:11:27 +0200

    update

--- assigment 1/src/App.js
+++ assigment 1/src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
           </UserOutput>
           {/* <UserInput changeUser={this.userNameHandler} /> */}
           <UserInput 
-            changeName={this.writeNameHandler}
+            changeName={this.writeNameHandler.bind(this)}
             userName={this.state.user[0].name}
           />
 
