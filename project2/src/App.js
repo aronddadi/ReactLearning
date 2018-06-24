7a07625608d0796e0fd46b1d19c07ae6070f53af
Aron Dadi <aron.d.dadi@gmail.com>
2018-06-24 14:50:33 +0200

    zz

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Radium, {StyleRoot} from 'radium'; 
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -60,16 +59,9 @@ class App extends Component {
 
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      color:'white',
-    };
 
     let persons = null;
+    let btnClass ='';
     // Conditionally shown content should be done in  javascript
     // Use maps to output lists
     if(this.state.showPersons){
@@ -85,27 +77,26 @@ class App extends Component {
              />
           })}
           </div>
-          
       );
-      style.backgroundColor = 'red';
-
+      
+      btnClass = classes.Red
     }
 
-    const classes=[]
+    const assignedClasses=[]
     if(this.state.persons.length<=2){
-      classes.push('red'); // Class => red
+      assignedClasses.push(classes.red); // Class => red
     }
     if(this.state.persons.length<=1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
     
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button 
-          style={style}
+        className={btnClass}
           onClick={ this.togglePersonsHandler}>Show Names</button>
           
         {persons}
